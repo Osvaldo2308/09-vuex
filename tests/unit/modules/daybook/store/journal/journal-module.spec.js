3fc0142a485f219e209728c46e81e654f6b1e40f
Osvaldo2308 <osvaldo245678@gmail.com>
2023-02-14 12:04:49 -0600

    last Update

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex'
 import journal from '@/modules/daybook/store/journal'
 import { journalState } from '../../../../mock-data/test-journal-state'
 
-
+import authApi from '@/api/authApi'
 
 const createVuexStore = (initialState) =>
 createStore ({
@@ -16,6 +16,15 @@ createStore ({
 
 describe('Vuex - Pruebas en el Journal Module', ()=>{
 
+
+    beforeAll(async( )=>{
+        const {data} = await authApi.post(':signInWithPassword', {
+            email: 'test@test.com',
+            password: '123456',
+            returnSecureToken: true
+        })
+        localStorage.setItem('idToken', data.idToken)
+    })
     //--------------------Basica--------------------
     test('Este es el estado inicial, debe de tener este state', ()=>{
         const store = createVuexStore (journalState)
